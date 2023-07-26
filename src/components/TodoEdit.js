177e9c8f870596e0da0aff056cf8b088458ad97b
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2023-07-26 20:39:20 +0700

    complete app todolist

--- src/components/TodoEdit.js
+++ src/components/TodoEdit.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
-export const TodoEdit = () => {
-  return <div>TodoEdit</div>;
+export const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        value={value}
+        className="todo-input"
+        placeholder="Update task "
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      <button className="todo-btn" type="submit">
+        Update Task
+      </button>
+    </form>
+  );
 };
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
-export const TodoForm = () => {
-  return <div>TodoForm</div>;
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addTodo(value);
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        value={value}
+        className="todo-input"
+        placeholder="What is the task today ? "
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      <button className="todo-btn" type="submit">
+        Add Task
+      </button>
+    </form>
+  );
 };
